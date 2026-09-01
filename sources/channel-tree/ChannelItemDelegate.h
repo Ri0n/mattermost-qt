d79bb59ab6d12150ed7b93c76f4d3ef55dfe6306
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-01 18:51:51 +0300

    Add sidebar channel delegate

---
+++ sources/channel-tree/ChannelItemDelegate.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <QStyledItemDelegate>
+
+namespace Mattermost {
+
+class ChannelItemDelegate final : public QStyledItemDelegate
+{
+    Q_OBJECT
+public:
+    explicit ChannelItemDelegate(QObject* parent = nullptr);
+
+    QSize sizeHint(const QStyleOptionViewItem& option,
+                   const QModelIndex& index) const override;
+    void paint(QPainter* painter,
+               const QStyleOptionViewItem& option,
+               const QModelIndex& index) const override;
+};
+
+} // namespace Mattermost
