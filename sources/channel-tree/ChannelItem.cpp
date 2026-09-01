83d41dd7f0911dfe87ca0cdfcc9b34af9e1dc75e
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-02 01:22:10 +0300

    Drop unused ChannelItem implementation change

--- sources/channel-tree/ChannelItem.cpp
+++ sources/channel-tree/ChannelItem.cpp
@@ -10,7 +10,7 @@
  *
  * Mattermost-QT is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * Mattermost-QT is distributed in the hope that it will be useful,
@@ -81,11 +81,6 @@ void ChannelItem::setMentioned(bool mentioned)
     if (widget) {
         widget->setMentioned(mentioned);
     }
-}
-
-void ChannelItem::setUnread(bool unread)
-{
-    setData(0, ChannelTree::ItemUnreadRole, unread);
 }
 
 void ChannelItem::setStatus(const QString& status)
