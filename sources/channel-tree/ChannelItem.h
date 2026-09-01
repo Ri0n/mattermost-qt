d1dd76d8ef3bd618a69e990d6eaddea1be91eebc
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-02 01:21:57 +0300

    Drop unused ChannelItem API change

--- sources/channel-tree/ChannelItem.h
+++ sources/channel-tree/ChannelItem.h
@@ -10,7 +10,7 @@
  *
  * Mattermost-QT is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * Mattermost-QT is distributed in the hope that it will be useful,
@@ -42,7 +42,6 @@ public:
     void setWidget (ChannelItemWidget* itemWidget);
     void setMuted (bool muted);
     void setMentioned (bool mentioned);
-    void setUnread (bool unread);
     void setStatus (const QString& status);
 protected:
     void addCommonContextMenuActions(QMenu& menu, BackendChannel& channel);
