ae2e81e6e75cc1706ea0f3d883849f02ad8107c5
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-02 01:56:25 +0300

    Add resizable sidebar splitter

--- sources/mainwindow.h
+++ sources/mainwindow.h
@@ -1,22 +1,3 @@
-/**
- * Copyright 2021, 2022 Lyubomir Filipov
- *
- * This file is part of Mattermost-QT.
- *
- * Mattermost-QT is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Mattermost-QT is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Mattermost-QT. if not, see https://www.gnu.org/licenses/.
- */
-
 #pragma once
 
 #include <memory>
@@ -28,6 +9,7 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+class QSplitter;
 class QSystemTrayIcon;
 class QTabWidget;
 class QTreeWidgetItem;
@@ -95,6 +77,7 @@ private:
 	ChooseEmojiDialogWrapper			chooseEmojiDialog;
 	QSet<const BackendChannel*>			channelsWithNewPosts;
 	Backend&							backend;
+	QSplitter*							sidebarSplitter = nullptr;
 	QTabWidget*							channelTabs = nullptr;
 	ChannelQuickList*					recentChannels = nullptr;
 	ChannelQuickList*					unreadChannels = nullptr;
