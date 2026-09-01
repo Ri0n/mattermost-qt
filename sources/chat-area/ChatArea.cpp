93b07fdb34db98b5b38d7cd74673cf65e4a20abf
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-01 23:23:38 +0300

    Avoid wildcard disconnect during ChatArea destruction

--- sources/chat-area/ChatArea.cpp
+++ sources/chat-area/ChatArea.cpp
@@ -5,7 +5,7 @@
  *
  * Mattermost-QT is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * Mattermost-QT is distributed in the hope that it will be useful,
@@ -473,7 +473,6 @@ ChatArea::ChatArea (Backend& backend, BackendChannel& channel, QString rootId, C
 
 ChatArea::~ChatArea()
 {
-	disconnect();
 	if (isThread)
 		parentArea->threadsAreas.remove(this);
 	delete ui;
