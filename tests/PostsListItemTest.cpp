64aee75e6c0d1599453ec202839e5495044a9874
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-02 20:57:46 +0300

    Cover transparent timeline gap item semantics [skip ci]

--- tests/PostsListItemTest.cpp
+++ tests/PostsListItemTest.cpp
@@ -12,15 +12,40 @@ private slots:
     void classifiesListItems()
     {
         QVERIFY(!PostsListWidget::isPostItem(nullptr));
+        QVERIFY(!PostsListWidget::isGapItem(nullptr));
 
         QListWidgetItem item;
         QVERIFY(!PostsListWidget::isPostItem(&item));
+        QVERIFY(!PostsListWidget::isGapItem(&item));
 
         item.setData(Qt::UserRole, ItemType::separator);
         QVERIFY(!PostsListWidget::isPostItem(&item));
+        QVERIFY(!PostsListWidget::isGapItem(&item));
 
         item.setData(Qt::UserRole, ItemType::post);
         QVERIFY(PostsListWidget::isPostItem(&item));
+        QVERIFY(!PostsListWidget::isGapItem(&item));
+
+        item.setData(Qt::UserRole, ItemType::gap);
+        QVERIFY(!PostsListWidget::isPostItem(&item));
+        QVERIFY(PostsListWidget::isGapItem(&item));
+    }
+
+    void gapCanBeGeometryOnly()
+    {
+        QListWidgetItem gap;
+        gap.setData(Qt::UserRole, ItemType::gap);
+        gap.setData(ItemRole::gapFirstIndex, 80);
+        gap.setData(ItemRole::gapCount, 637);
+        gap.setFlags(Qt::NoItemFlags);
+        gap.setSizeHint(QSize(0, 64000));
+
+        QVERIFY(PostsListWidget::isGapItem(&gap));
+        QCOMPARE(gap.text(), QString());
+        QVERIFY(gap.icon().isNull());
+        QCOMPARE(gap.background().style(), Qt::NoBrush);
+        QCOMPARE(gap.flags(), Qt::NoItemFlags);
+        QCOMPARE(gap.sizeHint().height(), 64000);
     }
 };
 
