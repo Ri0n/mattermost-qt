5b69b8c288033ee0b63c3eb9f7535c10f5aa7f76
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-02 20:28:35 +0300

    Add lazy thread timeline controller [skip ci]

---
+++ sources/chat-area/ThreadTimelineController.h
@@ -0,0 +1,49 @@
+/**
+ * @file ThreadTimelineController.h
+ * @brief Lazy/prefetched loading for large thread windows.
+ *
+ * Copyright 2026 Sergei Ilinykh
+ *
+ * This file is part of Mattermost-QT.
+ */
+
+#pragma once
+
+#include <cstdint>
+
+#include <QObject>
+#include <QPointer>
+#include <QString>
+
+namespace Mattermost {
+
+class ChatArea;
+class ThreadTimelineController;
+
+/** Created from ChatArea's late member initializer; returns null for channels. */
+ThreadTimelineController* createThreadTimelineController(ChatArea& area);
+
+class ThreadTimelineController : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ThreadTimelineController(ChatArea& area);
+
+private:
+    void start();
+    void requestNextPage();
+    void scheduleViewportCheck();
+    void checkViewport();
+
+    ChatArea& area;
+    QString rootId;
+    QString cursorPostId;
+    uint64_t cursorCreateAt = 0;
+    int expectedPostCount = 1;
+    int initialPagesRemaining = 0;
+    bool requestInFlight = false;
+    bool hasNext = true;
+    bool viewportCheckScheduled = false;
+};
+
+} // namespace Mattermost
