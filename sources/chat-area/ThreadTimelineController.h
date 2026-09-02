b2b24e3087f76487b12b01b47747896334a000a2
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-02 20:32:44 +0300

    Virtualize unloaded thread ranges [skip ci]

--- sources/chat-area/ThreadTimelineController.h
+++ sources/chat-area/ThreadTimelineController.h
@@ -14,8 +14,11 @@
 #include <QObject>
 #include <QPointer>
 #include <QString>
+#include <QTimer>
 
 #include "backend/PostTimeline.h"
+
+class QListWidgetItem;
 
 namespace Mattermost {
 
@@ -34,20 +37,29 @@ public:
 private:
     void start();
     void requestNextPage();
+    void requestSeek(int logicalIndex);
     void scheduleViewportCheck();
     void checkViewport();
+    void renderTimeline(const QString& focusPostId = QString(), bool focusAtTop = false);
+    void updateGapHeights();
+    void scheduleMeasuredHeightUpdate(const QString& postId);
+    QListWidgetItem* gapAtViewportCenter() const;
+    int logicalIndexInsideGap(const QListWidgetItem* gapItem) const;
 
     ChatArea& area;
     QString rootId;
     QString cursorPostId;
     uint64_t cursorCreateAt = 0;
     PostTimeline timeline;
+    QTimer seekTimer;
     int expectedPostCount = 1;
     int nextLogicalIndex = 0;
     int initialPagesRemaining = 0;
+    int pendingSeekIndex = -1;
     bool requestInFlight = false;
     bool hasNext = true;
     bool viewportCheckScheduled = false;
+    bool rebuilding = false;
 };
 
 } // namespace Mattermost
