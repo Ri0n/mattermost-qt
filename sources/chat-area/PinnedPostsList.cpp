f7b7d772653291c966f1f183a4552a8010f890e3
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-02 14:31:51 +0300

    Make pinned posts panel usable

--- sources/chat-area/PinnedPostsList.cpp
+++ sources/chat-area/PinnedPostsList.cpp
@@ -23,18 +23,47 @@
  */
 
 #include "PinnedPostsList.h"
-#include "ui_PinnedPostsList.h"
+
+#include <QDockWidget>
+#include <QHBoxLayout>
+#include <QKeySequence>
+#include <QLabel>
+#include <QShortcut>
+#include <QToolButton>
+#include <QVBoxLayout>
+
+#include "ChatArea.h"
 #include "post/PostWidget.h"
-#include <QDebug>
-#include <QResizeEvent>
+#include "ui_PinnedPostsList.h"
 
 namespace Mattermost {
 
 PinnedPostsList::PinnedPostsList(QWidget *parent)
 :QWidget(parent)
 ,ui(new Ui::PinnedPostsList)
+,chatArea(qobject_cast<ChatArea*>(parent))
 {
     ui->setupUi(this);
+
+    auto* header = new QWidget(this);
+    auto* headerLayout = new QHBoxLayout(header);
+    headerLayout->setContentsMargins(4, 2, 2, 2);
+    headerLayout->setSpacing(4);
+    headerLayout->addWidget(new QLabel(tr("Pinned messages"), header));
+    headerLayout->addStretch();
+
+    auto* closeButton = new QToolButton(header);
+    closeButton->setText(QStringLiteral("×"));
+    closeButton->setToolTip(tr("Close"));
+    closeButton->setAutoRaise(true);
+    headerLayout->addWidget(closeButton);
+    ui->verticalLayout->insertWidget(0, header);
+
+    connect(closeButton, &QToolButton::clicked, this, &PinnedPostsList::closePanel);
+
+    auto* escapeShortcut = new QShortcut(QKeySequence(Qt::Key_Escape), this);
+    escapeShortcut->setContext(Qt::WidgetWithChildrenShortcut);
+    connect(escapeShortcut, &QShortcut::activated, this, &PinnedPostsList::closePanel);
 }
 
 PinnedPostsList::~PinnedPostsList()
@@ -42,11 +71,57 @@ PinnedPostsList::~PinnedPostsList()
     delete ui;
 }
 
+void PinnedPostsList::closePanel()
+{
+    QWidget* widget = parentWidget();
+    while (widget) {
+        if (auto* dock = qobject_cast<QDockWidget*>(widget)) {
+            // ChatArea stores the dock in a QPointer, so deleting the floating
+            // panel also clears the owner's handle safely.
+            dock->deleteLater();
+            return;
+        }
+        widget = widget->parentWidget();
+    }
+
+    hide();
+}
+
 void PinnedPostsList::addPost (PostWidget* postWidget)
 {
-	QListWidgetItem* newItem = new QListWidgetItem();
-	ui->listWidget->addItem (newItem);
-	ui->listWidget->setItemWidget (newItem, postWidget);
+    if (!postWidget) {
+        return;
+    }
+
+    const QString postId = postWidget->post.id;
+
+    auto* rowWidget = new QWidget(ui->listWidget);
+    auto* rowLayout = new QVBoxLayout(rowWidget);
+    rowLayout->setContentsMargins(0, 0, 0, 4);
+    rowLayout->setSpacing(2);
+
+    auto* actionsLayout = new QHBoxLayout;
+    actionsLayout->setContentsMargins(0, 0, 2, 0);
+    actionsLayout->addStretch();
+
+    auto* goToButton = new QToolButton(rowWidget);
+    goToButton->setText(tr("Go to message"));
+    goToButton->setToolTip(tr("Show this message in the channel"));
+    actionsLayout->addWidget(goToButton);
+
+    rowLayout->addLayout(actionsLayout);
+    rowLayout->addWidget(postWidget);
+
+    QListWidgetItem* newItem = new QListWidgetItem();
+    ui->listWidget->addItem (newItem);
+    ui->listWidget->setItemWidget (newItem, rowWidget);
+
+    connect(goToButton, &QToolButton::clicked, this, [this, postId] {
+        if (chatArea) {
+            chatArea->goToPost(postId);
+        }
+        closePanel();
+    });
 }
 
 } /* namespace Mattermost */
