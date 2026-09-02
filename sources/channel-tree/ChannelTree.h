dd4e904ca9dc19f17460e3df7dabe286c5bd5e02
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-02 10:22:55 +0300

    Create chat areas only when opened

--- sources/channel-tree/ChannelTree.h
+++ sources/channel-tree/ChannelTree.h
@@ -10,7 +10,7 @@
  *
  * Mattermost-QT is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * Mattermost-QT is distributed in the hope that it will be useful,
@@ -99,6 +99,8 @@ private:
 	                                    const QString& displayName, bool collapsed);
 	ChannelItem* createChannelItem(Backend& backend, TeamItem& teamItem,
 	                               QTreeWidgetItem& categoryItem, BackendChannel& channel);
+	ChatArea* ensureChatArea(QTreeWidgetItem* item);
+	void activateChannelItem(QTreeWidgetItem* item);
 	void setCategoryCollapsed(QTreeWidgetItem* item, bool collapsed);
 	void setChannelMutedVisual(const QString& channelId, bool muted);
 	void setChannelMentionedVisual(const QString& channelId, bool mentioned);
