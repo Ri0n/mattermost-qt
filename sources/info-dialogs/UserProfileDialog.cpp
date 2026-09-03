cfceafe50174b01fdef533b81b7747fba499f643
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-03 18:00:12 +0300

    Scale profile avatars to full size [skip ci]

--- sources/info-dialogs/UserProfileDialog.cpp
+++ sources/info-dialogs/UserProfileDialog.cpp
@@ -5,7 +5,7 @@
  *
  * Mattermost-QT is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * Mattermost-QT is distributed in the hope that it will be useful,
@@ -37,7 +37,16 @@ UserProfileDialog::UserProfileDialog (const BackendUser& user, QWidget *parent)
 
     setWindowTitle ("Profile for " + user.getDisplayName() + " - Mattermost");
 
-    ui->avatar->setPixmap (user.avatar);
+    constexpr int ProfileAvatarSize = 128;
+    if (!user.avatar.isNull()) {
+        ui->avatar->setPixmap(user.avatar.scaled(ProfileAvatarSize,
+                                                  ProfileAvatarSize,
+                                                  Qt::KeepAspectRatio,
+                                                  Qt::SmoothTransformation));
+    } else {
+        ui->avatar->clear();
+    }
+    ui->avatar->setAlignment(Qt::AlignCenter);
     ui->fullnameValue->setText (user.first_name + " " + user.last_name);
     ui->nicknameValue->setText (getString (user.nickname));
     ui->usernameValue->setText (user.username);
