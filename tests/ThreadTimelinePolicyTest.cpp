6413dfa28d40e1f25912cbc4e288b445f99b74db
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-03 12:55:02 +0300

    Test thread live-reply and newest-edge policy [skip ci]

---
+++ tests/ThreadTimelinePolicyTest.cpp
@@ -0,0 +1,41 @@
+#include <QtTest>
+
+#include "chat-area/ThreadTimelinePolicy.h"
+
+using namespace Mattermost;
+
+class ThreadTimelinePolicyTest : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void rootCountAlreadyIncludesLiveReply()
+    {
+        QCOMPARE(threadExpectedCountAfterLiveReply(101, 102, false), 102);
+    }
+
+    void staleRootCountNeedsOneNewSlot()
+    {
+        QCOMPARE(threadExpectedCountAfterLiveReply(101, 101, false), 102);
+        QCOMPARE(threadExpectedCountAfterLiveReply(101, 99, false), 102);
+    }
+
+    void duplicateLiveReplyDoesNotGrowTimeline()
+    {
+        QCOMPARE(threadExpectedCountAfterLiveReply(101, 101, true), 101);
+        QCOMPARE(threadExpectedCountAfterLiveReply(101, 102, true), 102);
+    }
+
+    void shortPageConfirmsNewestBoundary()
+    {
+        QVERIFY(threadPageConfirmsNewestBoundary(false, QString(), 7, 30));
+        QVERIFY(threadPageConfirmsNewestBoundary(false, QString(), 0, 30));
+        QVERIFY(!threadPageConfirmsNewestBoundary(false, QString(), 30, 30));
+        QVERIFY(!threadPageConfirmsNewestBoundary(true, QString(), 7, 30));
+        QVERIFY(!threadPageConfirmsNewestBoundary(false, QStringLiteral("next"), 7, 30));
+    }
+};
+
+QTEST_APPLESS_MAIN(ThreadTimelinePolicyTest)
+
+#include "ThreadTimelinePolicyTest.moc"
