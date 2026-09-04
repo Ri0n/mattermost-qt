5f9222f7b08122d3b8fb411c783ff70b03e97251
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-05 02:37:41 +0300

    Consolidate thread cursor helpers into timeline service

--- sources/backend/PostTimelineService.cpp
+++ sources/backend/PostTimelineService.cpp
@@ -243,6 +243,28 @@ void PostTimelineService::loadThreadPage(BackendChannel& channel,
                QStringLiteral("down"), std::move(callback));
 }
 
+void PostTimelineService::loadThreadBefore(BackendChannel& channel,
+                                           const QString& rootId,
+                                           const QString& fromPost,
+                                           uint64_t fromCreateAt,
+                                           int perPage,
+                                           PageCallback callback)
+{
+    loadThread(channel, rootId, perPage, fromPost, fromCreateAt,
+               QStringLiteral("up"), std::move(callback));
+}
+
+void PostTimelineService::loadThreadAfter(BackendChannel& channel,
+                                          const QString& rootId,
+                                          const QString& fromPost,
+                                          uint64_t fromCreateAt,
+                                          int perPage,
+                                          PageCallback callback)
+{
+    loadThread(channel, rootId, perPage, fromPost, fromCreateAt,
+               QStringLiteral("down"), std::move(callback));
+}
+
 void PostTimelineService::loadThreadTail(BackendChannel& channel,
                                          const QString& rootId,
                                          int perPage,
