826b81512b2101cf73acb0397ac4aa920572eedf
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-06 01:38:36 +0300

    Add reusable presence avatar widgets

---
+++ sources/ui/PresenceAvatarLabel.h
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2026 Sergei Ilinykh
+ *
+ * This file is part of Mattermost-QT.
+ *
+ * Mattermost-QT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+#pragma once
+
+#include <QColor>
+#include <QLabel>
+#include <QPixmap>
+#include <QString>
+
+class QEvent;
+class QPaintEvent;
+class QResizeEvent;
+
+namespace Mattermost {
+
+/**
+ * QLabel-compatible avatar that renders the same circular presence treatment
+ * used in PostWidget. setPixmap() deliberately hides QLabel::setPixmap(): uic
+ * and existing callers keep their old API while the widget owns presentation.
+ */
+class PresenceAvatarLabel final : public QLabel
+{
+public:
+    explicit PresenceAvatarLabel(QWidget* parent = nullptr);
+
+    void setPixmap(const QPixmap& pixmap);
+    void setStatus(const QString& status);
+
+    static bool isPresenceStatus(const QString& text);
+
+protected:
+    void changeEvent(QEvent* event) override;
+    void paintEvent(QPaintEvent* event) override;
+    void resizeEvent(QResizeEvent* event) override;
+
+private:
+    void refreshPixmap();
+
+    QPixmap sourcePixmap;
+    QString presenceStatus;
+    QColor renderedBackground;
+};
+
+/**
+ * Compatibility label for the sidebar header. Existing MainWindow code writes
+ * online/away/dnd/offline as text; consume that value as avatar state instead
+ * of displaying a second textual presence indicator.
+ */
+class PresenceStatusLabel final : public QLabel
+{
+public:
+    explicit PresenceStatusLabel(QWidget* parent = nullptr);
+
+    void setText(const QString& text);
+};
+
+} // namespace Mattermost
