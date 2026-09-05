15ca5c924a372454da76d4d8a8d524f640da1b0d
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-05 23:41:20 +0300

    Add compact quoted post preview widget

---
+++ sources/chat-area/QuotedPostPreview.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <QFrame>
+#include <functional>
+
+class QLabel;
+class QMouseEvent;
+class QResizeEvent;
+
+namespace Mattermost {
+
+class BackendPost;
+
+class QuotedPostPreview final : public QFrame
+{
+public:
+    explicit QuotedPostPreview(QWidget* parent = nullptr, int maximumLines = 2);
+
+    void setPost(const BackendPost& post);
+    void setActivatedCallback(std::function<void()> callback);
+
+protected:
+    void changeEvent(QEvent* event) override;
+    void mouseReleaseEvent(QMouseEvent* event) override;
+    void resizeEvent(QResizeEvent* event) override;
+
+private:
+    void refreshPalette();
+    void refreshText();
+
+    QLabel* authorLabel = nullptr;
+    QLabel* messageLabel = nullptr;
+    QFrame* bar = nullptr;
+    QString fullText;
+    int maximumLines = 2;
+    std::function<void()> activatedCallback;
+};
+
+} // namespace Mattermost
