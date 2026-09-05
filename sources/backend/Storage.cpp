7ee4be7099d4df06d6aa3f182996c79c38a76f15
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-05 18:40:22 +0300

    Include iostream for Storage debug output

--- sources/backend/Storage.cpp
+++ sources/backend/Storage.cpp
@@ -10,7 +10,7 @@
  *
  * Mattermost-QT is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * Mattermost-QT is distributed in the hope that it will be useful,
@@ -23,6 +23,9 @@
  */
 
 #include "Storage.h"
+
+#include <iostream>
+
 #include "log.h"
 
 namespace Mattermost {
