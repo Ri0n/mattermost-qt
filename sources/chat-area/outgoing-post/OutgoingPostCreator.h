859ec563f95bd71f20d9924ade986b5c6bdebc5c
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-06 00:45:34 +0300

    Follow confirmed outgoing posts in chat log

--- sources/chat-area/outgoing-post/OutgoingPostCreator.h
+++ sources/chat-area/outgoing-post/OutgoingPostCreator.h
@@ -10,7 +10,7 @@
  *
  * Mattermost-QT is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * Mattermost-QT is distributed in the hope that it will be useful,
@@ -92,6 +92,7 @@ private:
 private:
 	Backend*							backend = nullptr;
 	BackendChannel*						channel = nullptr;
+	ChatLogWidget*						chatLogWidget = nullptr;
 	QLabel*								statusLabel = nullptr;
 	QPushButton*						attachButton = nullptr;
 	QPushButton*						addEmojiButton = nullptr;
