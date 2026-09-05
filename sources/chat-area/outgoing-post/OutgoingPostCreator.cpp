789fd281560c082e5e292cc84a92dc288e5cb14a
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-05 21:45:53 +0300

    Remove obsolete composer height signal

--- sources/chat-area/outgoing-post/OutgoingPostCreator.cpp
+++ sources/chat-area/outgoing-post/OutgoingPostCreator.cpp
@@ -10,7 +10,7 @@
  *
  * Mattermost-QT is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * Mattermost-QT is distributed in the hope that it will be useful,
@@ -495,7 +495,7 @@ void OutgoingPostCreator::updateEditorHeight()
 	if (height() != wantedHeight) {
 		setFixedHeight(wantedHeight);
 	}
-	emit heightChanged(wantedHeight);
+	updateGeometry();
 }
 
 bool OutgoingPostCreator::isCreatingPost()
@@ -517,5 +517,3 @@ void OutgoingPostCreator::setRootId(QString id)
 {
 	root_id = std::move(id);
 }
-
-} /* namespace Mattermost */
