cb8dda4b6e312c30c244214515185b9f5e94f4c5
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-05 15:57:37 +0300

    Add license header to IconUtils

--- sources/ui/IconUtils.h
+++ sources/ui/IconUtils.h
@@ -1,3 +1,24 @@
+/**
+ * Copyright 2026 Sergei Ilinykh
+ *
+ * Adapted from AnyKeep's IconUtils by the same author.
+ *
+ * This file is part of Mattermost-QT.
+ *
+ * Mattermost-QT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Mattermost-QT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Mattermost-QT. If not, see https://www.gnu.org/licenses/.
+ */
+
 #pragma once
 
 #include <QString>
