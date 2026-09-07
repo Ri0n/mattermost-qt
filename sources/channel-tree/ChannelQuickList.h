fdf81d5faaed94929c5ad0b9740c4418b145b9d0
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-07 23:36:22 +0300

    Turn Recent into followed conversation queue

--- sources/channel-tree/ChannelQuickList.h
+++ sources/channel-tree/ChannelQuickList.h
@@ -1,20 +1,28 @@
 /**
  * @file ChannelQuickList.h
- * @brief Flat channel index used by the Recent sidebar tab.
+ * @brief Followed conversation queue used by the Following sidebar tab.
  */
 
 #pragma once
 
 #include <cstdint>
 
+#include <QHash>
 #include <QMap>
 #include <QSet>
+#include <QTimer>
 #include <QTreeWidget>
+#include <QVector>
+
+#include "backend/ThreadFollowService.h"
+
+class QShowEvent;
 
 namespace Mattermost {
 
 class Backend;
 class BackendChannel;
+class BackendPost;
 class BackendUser;
 
 class ChannelQuickList : public QTreeWidget
@@ -22,32 +30,42 @@ class ChannelQuickList : public QTreeWidget
     Q_OBJECT
 public:
     enum Mode {
-        Recent,
+        Following,
     };
 
     explicit ChannelQuickList(QWidget* parent = nullptr);
 
-    void initialize(Backend& backend, Mode mode = Recent);
+    void initialize(Backend& backend, Mode mode = Following);
     void refresh();
+    void refreshThreads();
 
 signals:
     void channelSelected(const QString& channelId);
     void channelContextMenuRequested(const QString& channelId, const QPoint& globalPos);
 
-private:
-    struct RecentThreadTarget {
-        QString rootPostId;
-        QString fallbackPostId;
-        uint64_t interactionAt = 0;
-    };
+protected:
+    void showEvent(QShowEvent* event) override;
 
+private:
+    using ThreadSummary = ThreadFollowService::ThreadSummary;
+
+    void notePost(BackendChannel& channel, const BackendPost& post);
+    void clearSyntheticMentions(const QString& channelId);
+    void scheduleThreadRefresh();
+    void openThread(const ThreadSummary& thread);
+    QString threadLabel(const ThreadSummary& thread) const;
     void updateDirectUser(const BackendUser& user);
     void ensureDirectUserConnections(BackendChannel& channel);
 
     Backend* backend = nullptr;
     bool refreshing = false;
+    bool threadRefreshInFlight = false;
+    bool threadRefreshRequested = false;
+    QTimer threadRefreshTimer;
+    QVector<ThreadSummary> serverThreads;
+    QHash<QString, ThreadSummary> syntheticMentions;
+    QHash<QString, uint64_t> pendingSince;
     QMap<QString, QTreeWidgetItem*> channelItems;
-    QMap<QString, RecentThreadTarget> recentThreadTargets;
     QSet<QString> connectedUsers;
 };
 
