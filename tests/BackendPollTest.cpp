2769de2c9e87330c4bb85b7c4247680d9d3e37b2
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-08 10:45:30 +0300

    Cover poll definition refresh metadata lifetime

--- tests/BackendPollTest.cpp
+++ tests/BackendPollTest.cpp
@@ -14,6 +14,7 @@ class BackendPollTest : public QObject
 private slots:
     void metadataUsesFullOptionIndices();
     void metadataUsesCurrentAdminPermissionKey();
+    void definitionRefreshPreservesMetadata();
 };
 
 void BackendPollTest::metadataUsesFullOptionIndices()
@@ -22,7 +23,7 @@ void BackendPollTest::metadataUsesFullOptionIndices()
     for (int index = 0; index < 8; ++index) {
         QJsonObject action;
         action.insert(QStringLiteral("name"),
-                      index == 7 ? QStringLiteral("8. vuoksa-08")
+                      index == 7 ? QStringLiteral("selected-option")
                                  : QStringLiteral("option-%1").arg(index));
         // Put non-voting/admin actions before the selected answer. The metadata
         // index is still an index into the complete options array, not into a
@@ -39,7 +40,7 @@ void BackendPollTest::metadataUsesFullOptionIndices()
 
     QJsonObject metadata;
     metadata.insert(QStringLiteral("voted_answers"),
-                    QJsonArray {QStringLiteral("8. vuoksa-08")});
+                    QJsonArray {QStringLiteral("selected-option")});
     poll.fillMetadata(metadata);
 
     QCOMPARE(poll.metadata.ownVoteOptions.size(), 1);
@@ -64,6 +65,47 @@ void BackendPollTest::metadataUsesCurrentAdminPermissionKey()
     metadata.insert(QStringLiteral("admin_permission"), true);
     poll.fillMetadata(metadata);
     QVERIFY(poll.metadata.hasAdminPermissions);
+}
+
+void BackendPollTest::definitionRefreshPreservesMetadata()
+{
+    QJsonObject initialJson;
+    initialJson.insert(QStringLiteral("title"), QStringLiteral("old title"));
+    initialJson.insert(QStringLiteral("actions"), QJsonArray {
+        QJsonObject {{QStringLiteral("name"), QStringLiteral("one")},
+                     {QStringLiteral("id"), QStringLiteral("vote-one")}},
+        QJsonObject {{QStringLiteral("name"), QStringLiteral("two")},
+                     {QStringLiteral("id"), QStringLiteral("vote-two")}}
+    });
+    BackendPoll poll(QStringLiteral("poll-id"), initialJson);
+
+    QJsonObject metadata;
+    metadata.insert(QStringLiteral("can_manage_poll"), true);
+    metadata.insert(QStringLiteral("voted_answers"),
+                    QJsonArray {QStringLiteral("two")});
+    poll.fillMetadata(metadata);
+
+    QJsonObject refreshedJson;
+    refreshedJson.insert(QStringLiteral("title"), QStringLiteral("new title"));
+    refreshedJson.insert(QStringLiteral("text"), QStringLiteral("updated body"));
+    refreshedJson.insert(QStringLiteral("fields"), QJsonArray {
+        QJsonObject {{QStringLiteral("title"), QStringLiteral("one")},
+                     {QStringLiteral("value"), QStringLiteral("1 vote")}},
+        QJsonObject {{QStringLiteral("title"), QStringLiteral("two")},
+                     {QStringLiteral("value"), QStringLiteral("2 votes")}}
+    });
+    BackendPoll refreshed(QStringLiteral("poll-id"), refreshedJson);
+
+    poll.updateDefinition(refreshed);
+
+    QCOMPARE(poll.title, QStringLiteral("new title"));
+    QCOMPARE(poll.text, QStringLiteral("updated body"));
+    QVERIFY(poll.hasEnded);
+    QCOMPARE(poll.options.size(), 2);
+    QCOMPARE(poll.options.at(1).voters, QStringLiteral("2 votes"));
+    QVERIFY(poll.metadata.hasAdminPermissions);
+    QCOMPARE(poll.metadata.ownVoteOptions.size(), 1);
+    QCOMPARE(poll.metadata.ownVoteOptions.constFirst(), uint32_t(1));
 }
 
 QTEST_MAIN(BackendPollTest)
