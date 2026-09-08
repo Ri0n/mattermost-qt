af1196d305c44acc21709cbc96204449deea6380
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-09 00:00:57 +0300

    Attach poll team execution context

--- sources/chat-area/outgoing-post/NewPollDialog.cpp
+++ sources/chat-area/outgoing-post/NewPollDialog.cpp
@@ -10,7 +10,7 @@
  *
  * Mattermost-QT is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * Mattermost-QT is distributed in the hope that it will be useful,
@@ -174,6 +174,7 @@ BackendNewPollData NewPollDialog::getData()
     ret.maxVotes = ui->maxVotesValue->value();
 
     if (auto* creator = qobject_cast<OutgoingPostCreator*>(parentWidget())) {
+        ret.commandTeamId = creator->pollCommandTeamId();
         creator->armPollRealtimeAcknowledgement(ret);
     }
 
