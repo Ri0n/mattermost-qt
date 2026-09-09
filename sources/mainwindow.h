4ba72adbe35263f87f94b353ca4b03959f8122d1
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-09 17:18:19 +0300

    Drop legacy notification navigation entry point

--- sources/mainwindow.h
+++ sources/mainwindow.h
@@ -49,7 +49,6 @@ class ChannelQuickList;
 class NotificationManager;
 class PostCollectionView;
 class SettingsWindow;
-struct NotificationTarget;
 
 class MainWindow: public QMainWindow {
 	Q_OBJECT
@@ -80,7 +79,6 @@ protected:
 private:
 	void createMenu ();
 	void reload ();
-	void activateNotification (const NotificationTarget& target);
 	void setupChannelTabs ();
 	void refreshSidebarViews ();
 	void refreshChannelUnreadFilter ();
