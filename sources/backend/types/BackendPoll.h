6625437900983713d3ad731885fc8717c409e10a
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-09 13:59:46 +0300

    Track Matterpoll add-option visibility

--- sources/backend/types/BackendPoll.h
+++ sources/backend/types/BackendPoll.h
@@ -10,7 +10,7 @@
  *
  * Mattermost-QT is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * Mattermost-QT is distributed in the hope that it will be useful,
@@ -40,7 +40,8 @@ struct BackendPollOption {
  * Poll metadata. It is received separately - only when requested, or when a vote is made
  */
 struct BackendPollMetadata {
-	bool						hasAdminPermissions;
+	bool						hasAdminPermissions = false;
+	bool						allowsPublicAddOption = false;
 	QVector<uint32_t>			ownVoteOptions;
 };
 
