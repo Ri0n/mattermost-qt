bc9d93681a04bfedd90a8c0671e2dbf74781fe28
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-10 21:22:58 +0300

    Handle channel avatar profile clicks

--- sources/chat-area/ChatArea.h
+++ sources/chat-area/ChatArea.h
@@ -5,7 +5,7 @@
  *
  * Mattermost-QT is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * Mattermost-QT is distributed in the hope that it will be useful,
@@ -116,6 +116,10 @@ public:
 
 	/** Re-evaluate read progress from the current concrete timeline viewport. */
 	void refreshReadState ();
+
+private slots:
+    void on_userAvatar_clicked();
+
 private:
 	void changeEvent(QEvent* event) override;
 	void showEvent(QShowEvent* event) override;
