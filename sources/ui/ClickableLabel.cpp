da43c0fbcf2a3a109e88dd57ed2c2f146345d7d1
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-10 21:21:35 +0300

    Implement clickable label

---
+++ sources/ui/ClickableLabel.cpp
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2026 Sergei Ilinykh
+ *
+ * This file is part of Mattermost-QT.
+ */
+
+#include "ClickableLabel.h"
+
+#include <QMouseEvent>
+
+namespace Mattermost {
+
+ClickableLabel::ClickableLabel(QWidget* parent)
+    : QLabel(parent)
+{
+}
+
+void ClickableLabel::mouseReleaseEvent(QMouseEvent* event)
+{
+    QLabel::mouseReleaseEvent(event);
+    if (event && event->button() == Qt::LeftButton && rect().contains(event->pos())) {
+        emit clicked();
+    }
+}
+
+} // namespace Mattermost
